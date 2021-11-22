0ad14777aa0fe351b96e2c0027e2976136a7d0b9
Mahabir <mahabirg81@gmail.com>
2021-11-22 17:37:21 +0800

    made changes to about-us and created about-us page

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -115,38 +115,7 @@ const HomeCategoryScreen = {
             </span></a>
     </div>
 
-    
-        <div class="bg-white pt-8" id="aboutus">
-            <div class="container pt-8 px-6 mx-auto border-t border-gray-400">
-            <img src = "https://img.shein.com/images2/2018/11/29/15434606131933594897.jpg" height="70%"  width= "40%">
-                <h3 class="font-bold text-gray-900">About us</h3>
-        
-                <p class="mt-8 mb-8">GenSGStore is one of the leading one-stop e-commerce site that allows you to purchase all your
-                    fashion clothes at the comfort of your home. Our products are of very good materials and is value for money. We offer 
-                    free delivery for minimum purchases of $20. We also offer membership with 15% discount. We have been selling 
-                    fashion clothes online for the past 20 years and have receive plenty of customers feedback for our product and services.
-                    <br>
-                        
-                
-        
-                <p class="mb-8">GenSGStore was formed in 2001 by 3 entreprenuers. They ran a small business from their
-                    home, selling T-shirts. The business grew faster than they could have ever imagined.
-                    As the idea of a silk sleeping bag caught on, they began manufacturing “travel” clothing from the silk as
-                    well. And so the first apparel line was launched, soon followed by silk bedding. The company continued to
-                    grow.
-                    Then, at a fabric trade show in 2005, they met a young man who had a vision of his own. A new technology was
-                    emerging to convert wild-growing bamboo into incredibly soft, sumptuous fabric with a fraction of organic
-                    cotton’s environmental footprint.
-                    With the addition of bamboo clothing and bedding, the company experienced yet another surge in growth. 
-                    Now in its 20th year, ownership has transitioned to a pair of longtime employees with a vision to lead 
-                    GenSGStore into its fullest expression as a sustainable clothing, bedding, and nightwear brand with broad
-                    multigenerational appeal. GenSGStore is committed to fair trade practices from source to shelf, and still 
-                    thrilled each day by the adventure that lies ahead!</p>
-
-                    <img src="https://www.volusion.com/blog/content/images/2017/04/Getting-Started-with-Volusion-How-to-Build-Your-About-Us-Page.jpg" height="70%"  width= "40%">
-            </div>
-        
-            <div class="container mx-auto bg-white pt-8 border-t border-gray-400">
+           <br> <div class="container mx-auto bg-white pt-8 border-t border-gray-400">
                 <div class="container flex px-3 pt-8 ">
                     <div class="w-full mx-auto flex justify-between flex-wrap">
                         <div class="flex w-1/2">
